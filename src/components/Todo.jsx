d9a5a811c2cdf45704d69987e6dd7e7d9d95dfea
Paul Caoile <hyp3rsp8c3@gmail.com>
2023-09-05 09:27:06 -0400

    add removeTodo functionality

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -4,6 +4,10 @@ import PropTypes from 'prop-types';
 export default class Todo extends Component {
   constructor(props) {
     super(props);
+    this.handleRemoveTodo = this.handleRemoveTodo.bind(this);
+  }
+  handleRemoveTodo() {
+    this.props.removeTodo(this.props.id);
   }
 
   render() {
@@ -12,7 +16,7 @@ export default class Todo extends Component {
         <li>{ this.props.task }</li>
         <div className='todo-list-buttons'>
           <button>Edit</button>
-          <button>X</button>
+          <button onClick={ this.handleRemoveTodo }>X</button>
         </div>
       </div>
     );
@@ -20,5 +24,7 @@ export default class Todo extends Component {
 }
 
 Todo.propTypes = {
-  task: PropTypes.string
+  task: PropTypes.string,
+  id: PropTypes.string,
+  removeTodo: PropTypes.func
 };
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -8,15 +8,21 @@ export default class TodoList extends Component {
     this.state = { todos: [] };
     // this.state = { todos: [{ task: 'learn react' }, { task: 'time to sleep' }] };
     this.addNewTodo = this.addNewTodo.bind(this);
+    this.removeTodo = this.removeTodo.bind(this);
   }
   addNewTodo(newTodo) {
     this.setState((state) => ({
       todos: [...state.todos, newTodo]
     }));
   }
+  removeTodo(id) {
+    this.setState({
+      todos: this.state.todos.filter((todo) => todo.id !== id)
+    });
+  }
   render() {
     const renderTodos = this.state.todos.map(todo => (
-      <Todo key={ todo.id } task={ todo.task } />
+      <Todo key={ todo.id } id={ todo.id } task={ todo.task } removeTodo={ this.removeTodo } />
     ));
     return (
       <div>
